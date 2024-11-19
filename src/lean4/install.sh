0868f8a431c6d3b10c4a4d25866a4763b2acee7c
Herman Chau <kcaze@users.noreply.github.com>
2024-11-19 05:25:48 +0000

    One more try

--- src/lean4/install.sh
+++ src/lean4/install.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
+ELAN_SCRIPT_PATH="/usr/local/share/elan-init.sh"
 INSTALL_ELAN_SCRIPT_PATH="/usr/local/share/install_elan.sh"
+
+curl https://raw.githubusercontent.com/leanprover/elan/master/elan-init.sh -o "$ELAN_SCRIPT_PATH"
+chmod 755 "$ELAN_SCRIPT_PATH"
 
 tee "$INSTALL_ELAN_SCRIPT_PATH" > /dev/null \
 << EOF
 #!/bin/sh
 set -e
-curl https://raw.githubusercontent.com/leanprover/elan/master/elan-init.sh -o /usr/local/share/elan-init.sh
-chmod 755 /usr/local/share/elan-init.sh
-sh /usr/local/share/elan-init.sh -y
+sh "$ELAN_SCRIPT_PATH" -y
 EOF
 
 chmod 755 "$INSTALL_ELAN_SCRIPT_PATH"
