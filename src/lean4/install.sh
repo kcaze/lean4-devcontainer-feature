acc28193bf6b302e69c3f6f207f033ae30e180b5
Herman Chau <kcaze@users.noreply.github.com>
2024-11-19 04:20:12 +0000

    Change installation to postCreateCommand.

--- src/lean4/install.sh
+++ src/lean4/install.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+INSTALL_ELAN_SCRIPT_PATH="/usr/local/share/install_elan.sh"
+tee "$INSTALL_ELAN_SCRIPT_PATH" > /dev/null \
+<< EOF
+#!/bin/sh
 curl https://raw.githubusercontent.com/leanprover/elan/master/elan-init.sh -sSf | sh -s -- -y
+EOF
